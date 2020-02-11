503c60ff572d8dd2deb5037a84c370c1ce0e7662
Michael Hall <michael@mbh.sh>
2020-02-11 10:42:10 +0000

    remove lustre mount for singularity

--- analysis/assembly/scripts/submit_lsf.sh
+++ analysis/assembly/scripts/submit_lsf.sh
@@ -11,7 +11,7 @@ fi
 MEMORY=4000
 PROFILE="lsf"
 
-lustre_mount="-B /hps/nobackup/research/zi/"
+#lustre_mount="-B /hps/nobackup/research/zi/"
 module load singularity/3.5.0
 
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
@@ -19,7 +19,7 @@ bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
-        snakemake --profile "$PROFILE" \
-            --singularity-args "$lustre_mount" "$@"
+        snakemake --profile "$PROFILE" "$@"
+#            --singularity-args "$lustre_mount" "$@"
 
 exit 0
