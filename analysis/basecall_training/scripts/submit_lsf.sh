c1777af637980a665165cae8d657c434dcce9bcd
Michael Hall <mbhall88@gmail.com>
2020-03-12 16:39:03 +0000

    add submission script for training

---
+++ analysis/basecall_training/scripts/submit_lsf.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
+LOG_DIR=logs/
+
+if [[ ! -d "$LOG_DIR" ]]
+then
+    echo "Error: Log directory $LOG_DIR does not exist"
+    exit 1
+fi
+
+MEMORY=4000
+PROFILE="lsf"
+
+module load singularity/3.5.0
+
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+    -M "$MEMORY" \
+    -o "$LOG_DIR"/"$JOB_NAME".o \
+    -e "$LOG_DIR"/"$JOB_NAME".e \
+    -J "$JOB_NAME" \
+        snakemake --profile "$PROFILE" "$@"
+
+exit 0
