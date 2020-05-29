f8ce486f409178a8e35eac7bdfc7a6f6806afcc6
Michael Hall <michael@mbh.sh>
2020-05-29 14:41:11 +1000

    add cluster submission script

---
+++ data/H37Rv_PRG/scripts/submit_lsf.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
+LOG_DIR=logs/
+
+if [[ ! -d "$LOG_DIR" ]]
+then
+    echo "Error: Log directory $LOG_DIR does not exist"
+    exit 1
+fi
+
+MEMORY=8000
+THREADS=4
+PROFILE="lsf"
+
+module load singularity/3.5.0
+
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+    -M "$MEMORY" \
+    -n "$THREADS" \
+    -o "$LOG_DIR"/"$JOB_NAME".o \
+    -e "$LOG_DIR"/"$JOB_NAME".e \
+    -J "$JOB_NAME" \
+        snakemake --profile "$PROFILE" --local-cores "$THREADS" "$@"
+
+exit 0
