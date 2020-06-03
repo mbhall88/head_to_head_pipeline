cb4d682e4da2f55ab13c3603bf4481210f16b245
Michael Hall <michael@mbh.sh>
2020-06-03 16:54:29 +1000

    WIP: write script to assign lineages [#11]

--- data/H37Rv_PRG/scripts/assign_lineages.py
+++ data/H37Rv_PRG/scripts/assign_lineages.py
@@ -1,12 +1,15 @@
 import logging
 import re
+from itertools import starmap
+
 from collections import Counter, defaultdict
 from dataclasses import dataclass
-from typing import TextIO, Dict, Union, Optional, NamedTuple
+from typing import TextIO, Dict, Optional, NamedTuple, List
 
 import click
-from cyvcf2 import VCF
+from cyvcf2 import VCF, Variant
 
+Indices = List[int]
 LINEAGE_REGEX = re.compile(r"^(lineage)?(?P<major>[\w]+)\.?(?P<minor>.*)?$")
 
 
@@ -16,6 +19,44 @@ class RowError(Exception):
 
 class InvalidLineageString(Exception):
     pass
+
+
+class Genotype(NamedTuple):
+    allele1: int
+    allele2: int
+    phased: bool
+
+    def is_null(self) -> bool:
+        """Is the genotype null. i.e. ./."""
+        return self.allele1 == -1 and self.allele2 == -1
+
+    def is_hom(self) -> bool:
+        """Is the genotype homozygous"""
+        if self.is_null():
+            return False
+        if self.allele1 == -1 or self.allele2 == -1:
+            return True
+        return self.allele1 == self.allele2
+
+    def is_het(self) -> bool:
+        """Is the genotype heterozyhous"""
+        return not self.is_null() and not self.is_hom()
+
+    def is_hom_ref(self) -> bool:
+        """Is genotype homozygous reference?"""
+        return self.is_hom() and (self.allele1 == 0 or self.allele2 == 0)
+
+    def is_hom_alt(self) -> bool:
+        """Is genotype homozygous alternate?"""
+        return self.is_hom() and (self.allele1 > 0 or self.allele2 > 0)
+
+    def alt_index(self) -> Optional[int]:
+        """If the genotype is homozygous alternate, returns the 0-based index of the
+        alt allele in the alternate allele array.
+        """
+        if not self.is_hom_alt():
+            return None
+        return max(self.allele1, self.allele2) - 1
 
 
 @dataclass
@@ -38,7 +79,7 @@ class Lineage:
         return Lineage(major=major, minor=minor)
 
 
-class Variant(NamedTuple):
+class PanelVariant(NamedTuple):
     """A class that represents a lineage-defining variant."""
 
     lineage: Lineage
@@ -50,7 +91,7 @@ class Variant(NamedTuple):
     gene_coord: Optional[int] = None
 
     @staticmethod
-    def from_row(row: str, delim: str = ",") -> "Variant":
+    def from_row(row: str, delim: str = ",") -> "PanelVariant":
         """A Variant factory constructor to build a Variant object from a row in a
         delimited file.
         """
@@ -76,23 +117,86 @@ class Variant(NamedTuple):
         except (IndexError, ValueError):
             gene_coord = None
 
-        return Variant(lineage, position, ref, alt, locus_id, gene_name, gene_coord)
+        return PanelVariant(
+            lineage, position, ref, alt, locus_id, gene_name, gene_coord
+        )
+
+
+class Classifier:
+    """A class to handle validating and classifying VCF entries with respect to the
+    panel."""
+
+    def __init__(
+        self, index: Optional[Dict[int, PanelVariant]] = None, include_het: bool = False
+    ):
+        if index is None:
+            index = dict()
+        self.index = index
+        self.include_het = include_het
+
+    def _is_position_valid(self, pos: int) -> bool:
+        return pos in self.index
+
+    def is_variant_valid(self, variant: Variant) -> bool:
+        if not self._is_position_valid(variant.POS):
+            return False
+
+        panel_variant = self.index[variant.POS]
+        if panel_variant.ref != variant.REF:
+            logging.warning(
+                f"Reference allele {variant.REF} at position {variant.POS} does "
+                f"not match panel variant {panel_variant.ref} at that position. "
+                f"Skipping this position..."
+            )
+            return False
+
+        if panel_variant.alt not in variant.ALT:
+            logging.debug(
+                f"No samples contain the panel ALT variant at position "
+                f"{variant.POS}. Skipping..."
+            )
+            return False
+
+        return True
+
+    def samples_with_lineage_variant(self, variant: Variant) -> Indices:
+        """Returns the indices of samples in the variant that have the same alternate
+        allele as the panel variant for that position."""
+        indices = []
+        panel_variant = self.index.get(variant.POS, None)
+        if panel_variant is None:
+            return indices
+
+        for sample_idx, gt in enumerate(starmap(Genotype, variant.genotypes)):
+            if gt.is_hom_alt():
+                alt_idx = gt.alt_index()
+                alt_base = variant.ALT[alt_idx]
+                if alt_base == panel_variant.alt:
+                    indices.append(sample_idx)
+                    continue
+            if self.include_het and gt.is_het():
+                alt_idxs = [a - 1 for a in [gt.allele1, gt.allele2] if a > 0]
+                alt_bases = {variant.ALT[i] for i in alt_idxs}
+                if panel_variant.alt in alt_bases:
+                    indices.append(sample_idx)
+
+        return indices
 
 
 def load_panel(
     stream: TextIO, no_header: bool = True, delim: str = ","
-) -> Dict[int, Variant]:
+) -> Dict[int, PanelVariant]:
     """Creates a dictionary mapping a position to a Variant for each entry in the
     file that panel points to.
     """
     if not no_header:
         _ = next(stream)  # skip header
 
-    index: Dict[int, Variant] = dict()
+    index: Dict[int, PanelVariant] = dict()
     for row in map(str.rstrip, stream):
         if not row:
             continue
-        variant = Variant.from_row(row, delim=delim)
+        variant = PanelVariant.from_row(row, delim=delim)
         if variant.position in index:
             raise IndexError(f"Duplicate position {variant.position} in panel.")
         index[variant.position] = variant
@@ -133,13 +237,38 @@ def load_panel(
     show_default=True,
 )
 @click.option(
+    "--default-lineage",
+    help="Lineage to use when no panel variants are found for a sample.",
+    default="unknown",
+    show_default=True,
+)
+@click.option(
+    "--output-delim",
+    help="Delimiting character used in the output file.",
+    default=",",
+    show_default=True,
+)
+@click.option(
     "--no-header",
     help="Indicates there is no header line in the panel file.",
     is_flag=True,
 )
+@click.option(
+    "--include-het",
+    help="Consider heterozygous (with relevant ALT) as lineage-defining.",
+    is_flag=True,
+)
 @click.option("-v", "--verbose", help="Turns on debug-level logging.", is_flag=True)
 def main(
-    input: str, panel: str, output: TextIO, delim: str, no_header: bool, verbose: bool,
+    input: str,
+    panel: str,
+    output: TextIO,
+    delim: str,
+    default_lineage: str,
+    output_delim: str,
+    no_header: bool,
+    verbose: bool,
+    include_het: bool,
 ):
     """Call Mycobacterium tuberculosis lineages for samples in a VCF file."""
     log_level = logging.DEBUG if verbose else logging.INFO
@@ -152,31 +281,40 @@ def main(
         panel_index = load_panel(stream, no_header=no_header, delim=delim)
     logging.info(f"Loaded panel with {len(panel_index)} variants successfully.")
 
-    classification: Dict[str, Counter] = defaultdict(Counter)
-    with VCF(input) as vcf:
-        for variant in vcf:
-            if variant.POS not in panel_index:
-                continue
+    logging.info("Searching VCF for lineage-defining variants...")
+    classification: Dict[str, List[Lineage]] = defaultdict(list)
+    classifier = Classifier(panel_index, include_het=include_het)
+    vcf = VCF(input)
+    for variant in vcf:
+        if not classifier.is_variant_valid(variant):
+            continue
 
-            panel_variant = panel_index[variant.POS]
-            if panel_variant.ref != variant.REF:
-                logging.warning(
-                    f"Reference allele {variant.REF} at position {variant.POS} does "
-                    f"not match panel variant {panel_variant.ref} at that position. "
-                    f"Skipping this position..."
-                )
-                continue
+        samples_with_alt: Indices = classifier.samples_with_lineage_variant(variant)
+        panel_variant = panel_index[variant.POS]
+        for idx in samples_with_alt:
+            sample = vcf.samples[idx]
+            classification[sample].append(panel_variant.lineage)
+            logging.debug(f"{sample} has variant for {panel_variant.lineage}")
 
-            try:
-                alt_idx = variant.ALT.index(panel_variant.alt)
-            except ValueError:
-                logging.debug(
-                    f"No samples contain the panel ALT variant at position "
-                    f"{variant.POS}"
-                )
-                continue
+    logging.info("Calling lineages for samples based on found lineage variants...")
+    output_header = output_delim.join(["sample", "called_lineage", "found_lineages"])
+    print(output_header, file=output)
 
-            # todo: get genotypes and see if any are the alt we want
+    for sample in vcf.samples:
+        if sample not in classification:
+            # todo: what is a good default?
+            logging.warning(
+                f"No panel variants were found for {sample}. Defaulting to "
+                f"{default_lineage}"
+            )
+            print(output_delim.join([sample, default_lineage, ""]), file=output)
+            continue
+
+        found_lineages = classification[sample]
+        called_lineage = classifier.call_lineage(found_lineages)  # todo: implement method
+
+    logging.info("All done!")
+    vcf.close()
 
 
 if __name__ == "__main__":
--- data/H37Rv_PRG/scripts/test_assign_lineages.py
+++ data/H37Rv_PRG/scripts/test_assign_lineages.py
@@ -1,6 +1,15 @@
 import pytest
+from unittest.mock import patch
 from io import StringIO
-from assign_lineages import Variant, RowError, Lineage, InvalidLineageString, load_panel
+from assign_lineages import (
+    PanelVariant,
+    RowError,
+    Lineage,
+    InvalidLineageString,
+    load_panel,
+    Classifier,
+    Genotype,
+)
 
 
 class TestLineageFromStr:
@@ -33,21 +42,21 @@ class TestLineageFromStr:
         assert actual == expected
 
 
-class TestVariantFromRow:
+class TestPanelVariantFromRow:
     def test_emptyRow_raisesError(self):
         row = ""
         with pytest.raises(RowError):
-            Variant.from_row(row)
+            PanelVariant.from_row(row)
 
     def test_rowWithNoFields_raisesError(self):
         row = ",,,,"
         with pytest.raises(RowError):
-            Variant.from_row(row)
+            PanelVariant.from_row(row)
 
     def test_rowPositionFieldMissing_raisesError(self):
         row = "foo,,,,"
         with pytest.raises(ValueError):
-            Variant.from_row(row)
+            PanelVariant.from_row(row)
 
     def test_geneCoordIsntInt_returnsNone(self):
         row = ",".join(
@@ -64,8 +73,8 @@ class TestVariantFromRow:
             ]
         )
 
-        actual = Variant.from_row(row)
-        expected = Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", None)
+        actual = PanelVariant.from_row(row)
+        expected = PanelVariant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", None)
 
         assert actual == expected
 
@@ -84,8 +93,8 @@ class TestVariantFromRow:
             ]
         )
 
-        actual = Variant.from_row(row)
-        expected = Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", None)
+        actual = PanelVariant.from_row(row)
+        expected = PanelVariant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", None)
 
         assert actual == expected
 
@@ -94,8 +103,8 @@ class TestVariantFromRow:
             ["lineage1", "615938", "1104", "G/A", "368", "GAG/GAA", "E/E", "", "hemL",]
         )
 
-        actual = Variant.from_row(row)
-        expected = Variant(Lineage("1"), 615938, "G", "A", None, "hemL", 1104)
+        actual = PanelVariant.from_row(row)
+        expected = PanelVariant(Lineage("1"), 615938, "G", "A", None, "hemL", 1104)
 
         assert actual == expected
 
@@ -104,8 +113,8 @@ class TestVariantFromRow:
             ["lineage1", "615938", "1104", "G/A", "368", "GAG/GAA", "E/E", "foo", "",]
         )
 
-        actual = Variant.from_row(row)
-        expected = Variant(Lineage("1"), 615938, "G", "A", "foo", None, 1104)
+        actual = PanelVariant.from_row(row)
+        expected = PanelVariant(Lineage("1"), 615938, "G", "A", "foo", None, 1104)
 
         assert actual == expected
 
@@ -115,7 +124,7 @@ class TestVariantFromRow:
         )
 
         with pytest.raises(ValueError):
-            Variant.from_row(row)
+            PanelVariant.from_row(row)
 
     def test_tabAsDelim_returnsExpected(self):
         delim = "\t"
@@ -123,8 +132,8 @@ class TestVariantFromRow:
             ["lineage1", "615938", "1104", "G/A", "368", "GAG/GAA", "E/E", "foo", "",]
         )
 
-        actual = Variant.from_row(row, delim=delim)
-        expected = Variant(Lineage("1"), 615938, "G", "A", "foo", None, 1104)
+        actual = PanelVariant.from_row(row, delim=delim)
+        expected = PanelVariant(Lineage("1"), 615938, "G", "A", "foo", None, 1104)
 
         assert actual == expected
 
@@ -146,7 +155,7 @@ class TestLoadPanel:
 
         assert actual == expected
 
-    def test_noHeader_allVariantsInPanel(self):
+    def test_noHeader_allPanelVariantsInPanel(self):
         stream = StringIO(
             "lineage1,615938,1104,G/A,368,GAG/GAA,E/E,Rv0524,hemL\n"
             "lineage1.1,4404247,1056,G/A,352,CTG/CTA,L/L,Rv3915,-"
@@ -154,8 +163,12 @@ class TestLoadPanel:
 
         actual = load_panel(stream)
         expected = {
-            615938: Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104),
-            4404247: Variant(Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056),
+            615938: PanelVariant(
+                Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104
+            ),
+            4404247: PanelVariant(
+                Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056
+            ),
         }
 
         assert actual == expected
@@ -169,8 +182,12 @@ class TestLoadPanel:
 
         actual = load_panel(stream)
         expected = {
-            615938: Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104),
-            4404247: Variant(Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056),
+            615938: PanelVariant(
+                Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104
+            ),
+            4404247: PanelVariant(
+                Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056
+            ),
         }
 
         assert actual == expected
@@ -193,8 +210,12 @@ class TestLoadPanel:
 
         actual = load_panel(stream, delim=delim)
         expected = {
-            615938: Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104),
-            4404247: Variant(Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056),
+            615938: PanelVariant(
+                Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104
+            ),
+            4404247: PanelVariant(
+                Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056
+            ),
         }
 
         assert actual == expected
@@ -208,8 +229,296 @@ class TestLoadPanel:
 
         actual = load_panel(stream, no_header=False)
         expected = {
-            615938: Variant(Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104),
-            4404247: Variant(Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056),
+            615938: PanelVariant(
+                Lineage("1"), 615938, "G", "A", "Rv0524", "hemL", 1104
+            ),
+            4404247: PanelVariant(
+                Lineage("1", "1"), 4404247, "G", "A", "Rv3915", "-", 1056
+            ),
         }
+
+        assert actual == expected
+
+
+class TestClassifierIsVariantValid:
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_posNotInPanel_returnsFalse(self, mock_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mock_variant.POS = 4
+        mock_variant.REF = "G"
+
+        assert not classifier.is_variant_valid(mock_variant)
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_variantRefDoesntMatchPanel_returnsFalse(self, mock_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mock_variant.POS = 1
+        mock_variant.REF = "d"
+
+        assert not classifier.is_variant_valid(mock_variant)
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_panelVariantAltNotInVariantAlts_returnsFalse(self, mock_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mock_variant.POS = 1
+        mock_variant.REF = "G"
+        mock_variant.ALT = ["T", "C"]
+
+        assert not classifier.is_variant_valid(mock_variant)
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_panelVariantAltInVariantAlts_returnsTrue(self, mock_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mock_variant.POS = 1
+        mock_variant.REF = "G"
+        mock_variant.ALT = ["T", "A"]
+
+        assert classifier.is_variant_valid(mock_variant)
+
+
+class TestGenotypeIsNull:
+    def test_isNull_returnsTrue(self):
+        gt = Genotype(-1, -1, False)
+
+        assert gt.is_null()
+
+    def test_isHom_returnsFalse(self):
+        gt = Genotype(-1, 1, False)
+
+        assert not gt.is_null()
+
+
+class TestGenotypeIsHom:
+    def test_isHom_returnsTrue(self):
+        gt = Genotype(0, 0, False)
+
+        assert gt.is_hom()
+
+    def test_isHet_returnsFalse(self):
+        gt = Genotype(1, 0, False)
+
+        assert not gt.is_hom()
+
+    def test_isNull_returnsFalse(self):
+        gt = Genotype(-1, -1, False)
+
+        assert not gt.is_hom()
+
+    def test_oneAlleleIsNull_returnsTrue(self):
+        gt = Genotype(-1, 1, False)
+
+        assert gt.is_hom()
+
+
+class TestGenotypeIsHet:
+    def test_isHet_returnsTrue(self):
+        gt = Genotype(2, 1, False)
+
+        assert gt.is_het()
+
+    def test_isHom_returnsFalse(self):
+        gt = Genotype(0, 0, False)
+
+        assert not gt.is_het()
+
+    def test_oneAlleleIsNull_returnsFalse(self):
+        gt = Genotype(-1, 1, False)
+
+        assert not gt.is_het()
+
+    def test_isNull_returnsFalse(self):
+        gt = Genotype(-1, -1, False)
+
+        assert not gt.is_het()
+
+
+class TestGenotypeIsHomRef:
+    def test_isHomRef_returnsTrue(self):
+        gt = Genotype(0, 0, False)
+
+        assert gt.is_hom_ref()
+
+    def test_isHomRefOneNull_returnsTrue(self):
+        gt = Genotype(-1, 0, False)
+
+        assert gt.is_hom_ref()
+
+    def test_isHomAlt_returnsFalse(self):
+        gt = Genotype(2, 2, False)
+
+        assert not gt.is_hom_ref()
+
+
+class TestGenotypeIsHomAlt:
+    def test_isHomAlt_returnsTrue(self):
+        gt = Genotype(1, 1, False)
+
+        assert gt.is_hom_alt()
+
+    def test_isHomAltOneNull_returnsTrue(self):
+        gt = Genotype(-1, 3, False)
+
+        assert gt.is_hom_alt()
+
+    def test_isHomRef_returnsFalse(self):
+        gt = Genotype(0, 0, False)
+
+        assert not gt.is_hom_alt()
+
+
+class TestGenotypeAltIndex:
+    def test_isHomAlt_returnsIndex(self):
+        gt = Genotype(1, 1, False)
+
+        actual = gt.alt_index()
+        expected = 0
+
+        assert actual == expected
+
+    def test_isHomAltWithOneNull_returnsIndex(self):
+        gt = Genotype(-1, 5, False)
+
+        actual = gt.alt_index()
+        expected = 4
+
+        assert actual == expected
+
+    def test_isHomRef_returnsNone(self):
+        gt = Genotype(0, 0, False)
+
+        assert gt.alt_index() is None
+
+    def test_isHet_returnsNone(self):
+        gt = Genotype(0, 3, False)
+
+        assert gt.alt_index() is None
+
+
+class TestClassifierSamplesWithLineageVariant:
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_noSampleHasLineageVariant_returnsEmpty(self, mocked_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[0, 0, False], [1, 1, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = []
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_oneSampleHasLineageVariant_returnsOneIndex(self, mocked_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[0, 0, False], [2, 2, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = [1]
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_allSamplesHaveLineageVariant_returnsAllIndices(self, mocked_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[2, -1, False], [2, 2, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = [0, 1]
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_posNotInPanel_returnsEmpty(self, mocked_variant):
+        index = {
+            4: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[2, -1, False], [2, 2, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = []
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_hetSampleHasLineageVariantIncludeHet_returnsHetIndex(self, mocked_variant):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index, include_het=True)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[-1, -1, False], [2, 0, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = [1]
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_hetSampleDoesntHaveLineageVariantIncludeHet_returnsEmpty(
+        self, mocked_variant
+    ):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index, include_het=True)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A", "N"]
+        mocked_variant.genotypes = [[-1, -1, False], [1, 3, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = []
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_hetSampleHasLineageVariantDontIncludeHet_doesntReturnHetIndex(
+        self, mocked_variant
+    ):
+        index = {
+            1: PanelVariant(Lineage("1"), 1, "G", "A"),
+        }
+        classifier = Classifier(index, include_het=False)
+        mocked_variant.POS = 1
+        mocked_variant.REF = "G"
+        mocked_variant.ALT = ["T", "A"]
+        mocked_variant.genotypes = [[2, 2, False], [2, 0, False]]
+
+        actual = classifier.samples_with_lineage_variant(mocked_variant)
+        expected = [0]
 
         assert actual == expected
