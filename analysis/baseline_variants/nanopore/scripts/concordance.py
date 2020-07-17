70ca4c2f8b502f6d4b815747c970f572a058edc8
Michael Hall <michael@mbh.sh>
2020-07-17 15:20:37 +1000

    WIP: finished CSV output for concordance script

--- analysis/baseline_variants/nanopore/scripts/concordance.py
+++ analysis/baseline_variants/nanopore/scripts/concordance.py
@@ -1,9 +1,16 @@
 import logging
 from enum import Enum
-from typing import TextIO, Set, Optional
-from cyvcf2 import VCF, Variant
+from typing import TextIO, Set, Optional, NamedTuple, List
 
 import click
+from cyvcf2 import Variant, VCF
+
+
+class DummyVariant(NamedTuple):
+    POS: float
+
+
+DUMMY = DummyVariant(float("inf"))
 
 
 class Bed:
@@ -27,7 +34,48 @@ class Bed:
         return item in self.positions
 
 
+class Genotype(NamedTuple):
+    allele1: int
+    allele2: int
 
+    def is_null(self) -> bool:
+        """Is the genotype null. i.e. ./."""
+        return self.allele1 == -1 and self.allele2 == -1
+
+    def is_hom(self) -> bool:
+        """Is the genotype homozygous"""
+        if self.is_null():
+            return False
+        if self.allele1 == -1 or self.allele2 == -1:
+            return True
+        return self.allele1 == self.allele2
+
+    def is_het(self) -> bool:
+        """Is the genotype heterozyhous"""
+        return not self.is_null() and not self.is_hom()
+
+    def is_hom_ref(self) -> bool:
+        """Is genotype homozygous reference?"""
+        return self.is_hom() and (self.allele1 == 0 or self.allele2 == 0)
+
+    def is_hom_alt(self) -> bool:
+        """Is genotype homozygous alternate?"""
+        return self.is_hom() and (self.allele1 > 0 or self.allele2 > 0)
+
+    def alt_index(self) -> Optional[int]:
+        """If the genotype is homozygous alternate, returns the 0-based index of the
+        alt allele in the alternate allele array.
+        """
+        if not self.is_hom_alt():
+            return None
+        return max(self.allele1, self.allele2) - 1
+
+    @staticmethod
+    def from_arr(arr: List[int]) -> "Genotype":
+        alleles = [a for a in arr if type(a) is int]
+        if len(alleles) < 2:
+            alleles.append(-1)
+        return Genotype(*alleles)
 
 
 class Outcome(Enum):
@@ -40,11 +88,25 @@ class Outcome(Enum):
     DiffAlt = "DIFF_ALT"
     Masked = "MASKED"
     SkippedNull = "SKIPPED_NULL"
+    BothFailFilter = "BOTH_FAIL_FILTER"
+    AFailFilter = "A_FAIL_FILTER"
+    BFailFilter = "B_FAIL_FILTER"
+    BothHet = "BOTH_HET"
+    AHet = "A_HET"
+    BHet = "B_HET"
+    AMissingPos = "A_MISSING_POS"
+    BMissingPos = "B_MISSING_POS"
 
 
 class Classifier:
-    def __init__(self, mask: Optional[Bed] = None, skip_null: bool = False):
+    def __init__(
+        self,
+        mask: Optional[Bed] = None,
+        skip_null: bool = False,
+        apply_filter: bool = False,
+    ):
         self.skip_null = skip_null
+        self.apply_filter = apply_filter
 
         if mask is None:
             mask = Bed()
@@ -61,15 +123,43 @@ class Classifier:
         if pos in self.mask:
             return Outcome.Masked
 
-        a_is_null = a_variant.num_unknown > 0
-        b_is_null = b_variant.num_unknown > 0
-        if self.skip_null and (a_is_null or b_is_null):
+        if self.apply_filter:
+            if a_variant.FILTER and b_variant.FILTER:
+                return Outcome.BothFailFilter
+            if a_variant.FILTER:
+                return Outcome.AFailFilter
+            if b_variant.FILTER:
+                return Outcome.BFailFilter
+
+        a_gt = Genotype.from_arr(a_variant.genotypes[0])
+        b_gt = Genotype.from_arr(b_variant.genotypes[0])
+        if self.skip_null and (a_gt.is_null() or b_gt.is_null()):
             return Outcome.SkippedNull
 
-        if a_is_null and b_is_null:
-            return Outcome.TrueNull
+        if b_gt.is_null():
+            return Outcome.TrueNull if a_gt.is_null() else Outcome.FalseNull
 
+        if a_gt.is_het() and b_gt.is_het():
+            return Outcome.BothHet
+        elif a_gt.is_het():
+            return Outcome.AHet
+        elif b_gt.is_het():
+            return Outcome.BHet
 
+        if b_gt.is_hom_ref():
+            return Outcome.TrueRef if a_gt.is_hom_ref() else Outcome.FalseRef
+
+        if b_gt.is_hom_alt():
+            if not a_gt.is_hom_alt():
+                return Outcome.FalseAlt
+
+            a_base = a_variant.ALT[a_gt.alt_index()]
+            b_base = b_variant.ALT[b_gt.alt_index()]
+
+            return Outcome.TrueAlt if a_base == b_base else Outcome.DiffAlt
+
+        # can't think of a way we could get here...
+        raise NotImplementedError(f"Could not classify variants at position {pos}")
 
 
 @click.command()
@@ -109,6 +199,13 @@ class Classifier:
     help="If either VCF has a NULL call at a position, skip classification.",
     is_flag=True,
 )
+@click.option(
+    "--apply-filter/--no-apply-filter",
+    "-f/-F",
+    default=True,
+    show_default=True,
+    help="Adhere to the VCF FILTER column.",
+)
 @click.option("-v", "--verbose", help="Turns on debug-level logging.", is_flag=True)
 def main(
     truth_vcf: str,
@@ -116,6 +213,7 @@ def main(
     bedfile: str,
     csv: TextIO,
     skip_null: bool,
+    apply_filter: bool,
     verbose: bool,
 ):
     """Calculate concordance between two VCF files. An assumption is made that both VCFs
@@ -134,7 +232,44 @@ def main(
         logging.info("No mask given. Classifying all positions...")
         mask = Bed()
 
-    classifier = Classifier(mask=mask, skip_null=skip_null)
+    classifier = Classifier(mask=mask, skip_null=skip_null, apply_filter=apply_filter)
+
+    logging.info("Classifying variants...")
+    print("pos,classification", file=csv)
+    a_vcf = VCF(truth_vcf)
+    a_variant = next(a_vcf, DUMMY)
+    a_exhausted = a_variant.POS == float("inf")
+    b_vcf = VCF(query_vcf)
+    b_variant = next(b_vcf, DUMMY)
+    b_exhausted = b_variant.POS == float("inf")
+
+    while (not a_exhausted) and (not b_exhausted):
+        if a_variant.POS == b_variant.POS:
+            classification = classifier.classify(a_variant, b_variant)
+            print(f"{a_variant.POS},{classification.value}", file=csv)
+            a_variant = next(a_vcf, DUMMY)
+            a_exhausted = a_variant.POS == float("inf")
+            b_variant = next(b_vcf, DUMMY)
+            b_exhausted = b_variant.POS == float("inf")
+            continue
+
+        if a_variant.POS > b_variant.POS:
+            classification = (
+                Outcome.Masked if b_variant.POS in mask else Outcome.AMissingPos
+            )
+            print(f"{b_variant.POS},{classification.value}", file=csv)
+            b_variant = next(b_vcf, DUMMY)
+            b_exhausted = b_variant.POS == float("inf")
+            continue
+
+        if a_variant.POS < b_variant.POS:
+            classification = (
+                Outcome.Masked if a_variant.POS in mask else Outcome.BMissingPos
+            )
+            print(f"{a_variant.POS},{classification.value}", file=csv)
+            a_variant = next(a_vcf, DUMMY)
+            a_exhausted = a_variant.POS == float("inf")
+            continue
 
 
 if __name__ == "__main__":
--- analysis/baseline_variants/nanopore/scripts/test_concordance.py
+++ analysis/baseline_variants/nanopore/scripts/test_concordance.py
@@ -36,9 +36,9 @@ class TestClassify:
         pos = 2
         classifier = Classifier(skip_null=True)
         mocked_avariant.POS = pos
-        mocked_avariant.num_unknown = 1
+        mocked_avariant.genotypes = [[-1]]
         mocked_bvariant.POS = pos
-        mocked_bvariant.num_unknown = 0
+        mocked_bvariant.genotypes = [[0]]
 
         actual = classifier.classify(mocked_avariant, mocked_bvariant)
         expected = Outcome.SkippedNull
@@ -47,15 +47,13 @@ class TestClassify:
 
     @patch("cyvcf2.Variant", autospec=True, create=True)
     @patch("cyvcf2.Variant", autospec=True, create=True)
-    def test_bothHaveNull_returnsTrueNull(
-            self, mocked_avariant, mocked_bvariant
-    ):
+    def test_bothHaveNull_returnsTrueNull(self, mocked_avariant, mocked_bvariant):
         pos = 2
         classifier = Classifier(skip_null=False)
         mocked_avariant.POS = pos
-        mocked_avariant.num_unknown = 1
+        mocked_avariant.genotypes = [[-1, -1]]
         mocked_bvariant.POS = pos
-        mocked_bvariant.num_unknown = 1
+        mocked_bvariant.genotypes = [[-1]]
 
         actual = classifier.classify(mocked_avariant, mocked_bvariant)
         expected = Outcome.TrueNull
@@ -64,17 +62,184 @@ class TestClassify:
 
     @patch("cyvcf2.Variant", autospec=True, create=True)
     @patch("cyvcf2.Variant", autospec=True, create=True)
-    def test_bHasNullOnly_returnsFalseNull(
-            self, mocked_avariant, mocked_bvariant
-    ):
+    def test_bHasNullOnly_returnsFalseNull(self, mocked_avariant, mocked_bvariant):
         pos = 2
         classifier = Classifier(skip_null=False)
         mocked_avariant.POS = pos
-        mocked_avariant.num_unknown = 0
+        mocked_avariant.genotypes = [[1, -1]]
         mocked_bvariant.POS = pos
-        mocked_bvariant.num_unknown = 1
+        mocked_bvariant.genotypes = [[-1]]
 
         actual = classifier.classify(mocked_avariant, mocked_bvariant)
         expected = Outcome.FalseNull
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bothRef_returnsTrueRef(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(skip_null=False)
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[0, -1]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[0, False]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.TrueRef
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bIsRef_returnsFalseRef(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(skip_null=False)
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[1, -1]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[0, False]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.FalseRef
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_aIsRefBIsAlt_returnsFalseAlt(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(skip_null=False)
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[0, 0]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[3]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.FalseAlt
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bothAlt_returnsTrueAlt(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(skip_null=False)
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[1, 1]]
+        mocked_avariant.ALT = ["C"]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[1]]
+        mocked_bvariant.ALT = ["C"]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.TrueAlt
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bothAltButDifferent_returnsDiffAlt(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(skip_null=False)
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[1, 1]]
+        mocked_avariant.ALT = ["C"]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[1]]
+        mocked_bvariant.ALT = ["A"]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.DiffAlt
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bothFailFilter_returnsBothFailFilter(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(apply_filter=True)
+        mocked_avariant.POS = pos
+        mocked_avariant.FILTER = 'b1'
+        mocked_bvariant.POS = pos
+        mocked_bvariant.FILTER = 'f0.90;z'
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.BothFailFilter
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_aFailFilter_returnsAFailFilter(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(apply_filter=True)
+        mocked_avariant.POS = pos
+        mocked_avariant.FILTER = 'b1'
+        mocked_bvariant.POS = pos
+        mocked_bvariant.FILTER = None
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.AFailFilter
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bFailFilter_returnsBFailFilter(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier(apply_filter=True)
+        mocked_avariant.POS = pos
+        mocked_avariant.FILTER = None
+        mocked_bvariant.POS = pos
+        mocked_bvariant.FILTER = "foo;bar"
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.BFailFilter
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bothHet_returnsBothHet(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier()
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[0, 1]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[0, 1]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.BothHet
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_aIsHet_returnsAHet(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier()
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[0, 1]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[0]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.AHet
+
+        assert actual == expected
+
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    @patch("cyvcf2.Variant", autospec=True, create=True)
+    def test_bIsHet_returnsBHet(self, mocked_avariant, mocked_bvariant):
+        pos = 2
+        classifier = Classifier()
+        mocked_avariant.POS = pos
+        mocked_avariant.genotypes = [[0, 0]]
+        mocked_bvariant.POS = pos
+        mocked_bvariant.genotypes = [[0, 1]]
+
+        actual = classifier.classify(mocked_avariant, mocked_bvariant)
+        expected = Outcome.BHet
 
         assert actual == expected
