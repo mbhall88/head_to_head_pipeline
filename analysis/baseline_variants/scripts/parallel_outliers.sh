4d97baf0548fa329fc3878d03df6978c8c4feb8a
Michael Hall <michael@mbh.sh>
2020-08-04 13:13:10 +1000

    add missing python script param

--- analysis/baseline_variants/scripts/parallel_outliers.sh
+++ analysis/baseline_variants/scripts/parallel_outliers.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 compass_dir="/hps/nobackup/research/zi/projects/tech_wars/analysis/baseline_variants/illumina/gvcfs/"
 mask="/hps/nobackup/research/zi/projects/tech_wars/analysis/baseline_variants/resources/compass-mask.bed"
-indir="$1"
-outdir="$2"
-threads="$3"
+pyscript="$1"
+indir="$2"
+outdir="$3"
+threads="$4"
 
 for outlier in "mada_2-46" "R27006" "R13303" "mada_1-34" "mada_1-50"
 do
@@ -12,6 +13,6 @@ a="${compass_dir}/${outlier}.compass.vcf.gz"
 outlier_dir="${outdir}/$outlier"
 mkdir -p "$outlier_dir"
 fd -j "$threads" -HI \
-    -x python -f -a "$a" -b '{}' -m "$mask" -c "${outlier_dir}/"'{/.}'".concordance.csv" -j "${outlier_dir}/"'{/.}'".concordance.json" \; \
+    -x python "$pyscript" -f -a "$a" -b '{}' -m "$mask" -c "${outlier_dir}/"'{/.}'".concordance.csv" -j "${outlier_dir}/"'{/.}'".concordance.json" \; \
     'snps.filtered.bcf$' "$indir"
 done
