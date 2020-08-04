9cd71ffbd5ddfd019101203473f6c89b3dc51d70
Michael Hall <michael@mbh.sh>
2020-08-04 16:54:41 +1000

    fix: use outlier nanopore vcf instead of compass [#40]

--- analysis/baseline_variants/scripts/parallel_outliers.sh
+++ analysis/baseline_variants/scripts/parallel_outliers.sh
@@ -9,10 +9,10 @@ threads="$4"
 for outlier in "mada_2-46" "R27006" "R13303" "mada_1-34" "mada_1-50"
 do
 echo "Running concordance for $outlier"
-a="${compass_dir}/${outlier}.compass.vcf.gz"
+b="${indir}/*/${outlier}.snps.filtered.bcf"
 outlier_dir="${outdir}/$outlier"
 mkdir -p "$outlier_dir"
 fd -j "$threads" -HI \
-    -x python "$pyscript" -f -a "$a" -b '{}' -m "$mask" -c "${outlier_dir}/"'{/.}'".concordance.csv" -j "${outlier_dir}/"'{/.}'".concordance.json" \; \
-    'snps.filtered.bcf$' "$indir"
+    -x python "$pyscript" -f -a '{}' -b "$b" -m "$mask" -c "${outlier_dir}/"'{/.}'".concordance.csv" -j "${outlier_dir}/"'{/.}'".concordance.json" \; \
+    'vcf.gz$' "$compass_dir"
 done
