b13082980ed6a1b7907295fc80c13c26040e094d
Michael Hall <michael@mbh.sh>
2020-08-21 02:39:25 +0100

    fixes for creating vcf ref [#52]

--- data/H37Rv_PRG/scripts/create_vcf_ref.py
+++ data/H37Rv_PRG/scripts/create_vcf_ref.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 outstream = open(snakemake.output.vcf_ref, "w")
+loci_dir = Path(snakemake.input.loci_info).parent
 
 with open(snakemake.input.loci_info) as csvfile:
+    _ = next(csvfile)  # skip header
     for row in map(str.rstrip, csvfile):
         fields = row.split(",")
-        loci_fasta = Path(fields[0])
-        outstream.write(loci_fasta.read_text())
+        loci_fasta = loci_dir / fields[0]
+        print(loci_fasta.read_text(), file=outstream)
 
 outstream.close()
