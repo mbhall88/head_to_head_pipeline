dbb6346f064a13a93c03143acf5e245345589588
Michael Hall <michael@mbh.sh>
2021-01-20 15:47:19 +1000

    change method for deduplicating fasta

--- analysis/pandora_variants/scripts/deduplicate_fasta.sh
+++ analysis/pandora_variants/scripts/deduplicate_fasta.sh
@@ -3,11 +3,11 @@ set -eu
 
 infile="$1"
 infilename=$(basename "$infile")
+prg_name="${infilename%%.*}"
 outdir="$2"
-outfile="${outdir}/${infilename%%.*}.deduplicated.fa"
+outfile="${outdir}/${prg_name}.deduplicated.fa"
 
-# snippet taken from https://www.biostars.org/p/3003/#3008
-sed -e '/^>/s/$/@/' -e 's/^>/#/' "$infile"  |\
-tr -d '\n' | tr "#" "\n" | tr "@" "\t" |\
-sort -u -t '  ' -f -k 2,2  |\
-sed -e 's/^/>/' -e 's/\t/\n/' > "$outfile"
+# snippet taken from https://www.biostars.org/p/3003
+STATS=$(fastx_collapser -i "$infile" -o "$outfile" -v 2>&1)
+
+printf "====\n%s stats:\n%s\n" "$prg_name" "$STATS" >&2
