4ed0c608954eeaddc8a15755fa534cb04ac5192e
Michael Hall <michael@mbh.sh>
2021-01-22 15:28:06 +1000

    update make_prg script to use new subcommand

--- data/H37Rv_PRG/scripts/build_local_prgs.sh
+++ data/H37Rv_PRG/scripts/build_local_prgs.sh
@@ -6,7 +6,7 @@ nesting_lvl="$3"
 match_len="$4"
 logfile="$5"
 
-make_prg prg_from_msa --max_nesting "$nesting_lvl" \
+make_prg from_msa --max_nesting "$nesting_lvl" \
     --min_match_length "$match_len" \
     --prefix "$out_prefix" \
     "$msa" 2>> "$logfile"
