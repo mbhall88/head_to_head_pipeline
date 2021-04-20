16e9d4af0192fa9c81ab00b67981ba60f47c140b
Michael Hall <michael@mbh.sh>
2021-04-20 15:07:15 +1000

    add rules to download and filter panel

---
+++ analysis/resistance_prediction/workflow/rules/drprg.smk
@@ -0,0 +1,29 @@
+rule download_panel:
+    output:
+        panel=RESULTS / "drprg/panel/panel.tsv",
+    resources:
+        mem_mb=int(0.5 * GB),
+    log:
+        LOGS / "download_panel.log",
+    params:
+        url=config["mykrobe_panel_url"],
+    shell:
+        "wget -O {output.panel} {params.url} 2> {log}"
+
+
+rule filter_panel:
+    input:
+        panel=rules.download_panel.output.panel,
+    output:
+        panel=RESULTS / "drprg/panel/panel.filtered.tsv",
+    log:
+        LOGS / "filter_panel.log",
+    resources:
+        mem_mb=int(0.5 * GB),
+    params:
+        exclude=["pncA", "katG"],
+        frameshift_lengths=[1, 2],
+    container:
+        CONTAINER["conda"]
+    script:
+        SCRIPTS / "filter_panel.py"
---
+++ analysis/resistance_prediction/workflow/scripts/filter_panel.py
@@ -0,0 +1,46 @@
+import sys
+
+sys.stderr = open(snakemake.log[0], "w")
+
+import re
+from typing import Set
+from dataclasses import dataclass
+
+frameshift_lengths: Set[int] = set(snakemake.params.frameshift_lengths)
+exclude_frameshifts_in: Set[str] = set(snakemake.params.exclude)
+
+
+@dataclass
+class Variant:
+    before: str
+    pos: int
+    after: str
+
+    @staticmethod
+    def from_str(var: str) -> "Variant":
+        before, pos, after = re.split(r"(-?\d+)", maxsplit=1)
+        return Variant(before=before, after=after, pos=int(pos))
+
+    def is_frameshift(self) -> bool:
+        len_diff = len(self.before) - len(self.after)
+        return abs(len_diff) in frameshift_lengths
+
+
+num_filtered = 0
+
+with open(snakemake.input.panel) as istream, open(
+    snakemake.output.panel, "w"
+) as ostream:
+    for row in map(str.rstrip, istream):
+        gene, var, alpha = row.split("\t")[:3]
+        is_dna = alpha == "DNA"
+        if (
+            gene in exclude_frameshifts_in
+            and is_dna
+            and Variant.from_str(var).is_frameshift()
+        ):
+            num_filtered += 1
+            continue
+        print(row, file=ostream)
+
+print(f"Filtered out {num_filtered} rows", file=sys.stderr)
